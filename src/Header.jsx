772de970a222c72b59eebdf24291565d651c4138
kabirsumn <kabirinnow26@gmail.com>
2024-02-14 10:42:02 +0600

    'Header

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header() {
+  return (
+    <header>
+      <nav><a href="#">Home</a></nav>
+      <nav><a href="#">Services</a></nav>
+      <nav><a href="#">About</a></nav>
+      <nav><a href="#">Contact</a></nav>
+    </header>
+  );
+}
+
+
+export default Header;
